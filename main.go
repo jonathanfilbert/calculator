d7ccd17111b31f04bdc43953e4579ebcfcdfaaad
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2021-07-06 11:05:22 +0700

    feat: first exercise: calculator

--- main.go
+++ main.go
@@ -2,22 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 func calculatorHandler(w http.ResponseWriter, req *http.Request) {
 
-	numA := req.URL.Query().Get("num_a")
-	numB := req.URL.Query().Get("num_b")
+	numA, _ := strconv.Atoi(req.URL.Query().Get("num_a"))
+	numB, _ := strconv.Atoi(req.URL.Query().Get("num_b"))
 	operator := req.URL.Query().Get("operator")
+	result := 0
 
-	fmt.Fprintf(w, fmt.Sprint(numA, numB, operator))
+	switch operator {
+	case "plus":
+		result = numA + numB
+	case "minus":
+		result = numA - numB
+	case "times":
+		result = numA * numB
+	case "divide":
+		result = numA / numB
+	case "power":
+		result = int(math.Pow(float64(numA), float64(numB)))
+	}
+
+	fmt.Fprintf(w, fmt.Sprint(result))
 }
 
 func main() {
+	// Declaration variables
+	port := ":8989"
 
 	http.HandleFunc("/calculator", calculatorHandler)
 
 	fmt.Println("serving HTTP...")
-	http.ListenAndServe(":8989", nil)
+	http.ListenAndServe(port, nil)
+
 }
